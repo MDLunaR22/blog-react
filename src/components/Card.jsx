20ddd10ed68edd409247dee6a234c36ef964c8fe
mluna-2021312 <mluna-2021312@kinal.edu.gt>
2023-05-02 17:40:35 -0600

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -19,7 +19,7 @@ export const Card = () => {
         }],
         descripcion: 'Todos los proyectos que aparecen en el drive son proyectos o ejercicios de refuerzo que se hicieron durante mi ciclo escolar de 4to diversificado y fue muy buena experiencia ya que supe que era ser un programador o comenzar a entrar en ese ambito. A continuacion se muestra un boton donde podra acceder a un drive donde esta cierta parte de los proyectos trabajados.',
         title: '4to diversificado',
-        img: '../../public/img/java.jpg',
+        img: 'java.jpg',
     },
     {
         id: 2,
@@ -36,7 +36,7 @@ export const Card = () => {
         }],
         descripcion: 'En este grado fue algo dificil mantenerme ya que todo lo aprendido era complicado, eran nuevas cosas, un nuevo mundo y algo muy extenso, que siempre tiene cosas nueva, en ese año aumente mis habilidades como programador ya que no solo requeria aprender de los componentes sino tambien utilizar la logica y ser intuitivo para que todo funcione correctamente a continuacion mostrare el codigo de 2 proyectos que se hicieron ese año.',
         title: '5to diversificado',
-        img: '../../public/img/FXScene.jpg',
+        img: 'FXScene.jpg',
     },
     {
         id: 3,
@@ -49,7 +49,7 @@ export const Card = () => {
         }],
         descripcion: 'Ultimo ciclo escolar y todo es tan diferente, ahora estamos viendo como utilizar react native con base de datos mongoDB y es algo muy extenso y muy confortante saber como es que funciona todo esto de una pagina web, a continuación el ejemplo del codigo.',
         title: '6to diversificado',
-        img: '../../public/img/noexmon.jpeg',
+        img: 'noexmon.jpeg',
     }]
     return (
         <>
@@ -58,7 +58,7 @@ export const Card = () => {
                     return (
                         <div className="row text-center justify-content-center pb-4 pt-4" key={e.id}>
                             <div className="col-md-6 mb-md-0 p-md-4">
-                                <img src={`${e.img}`} className="w-75" alt="No se encuentra"></img>
+                                <img src={`../../public/img/${e.img}`} className="w-75" alt="No se encuentra"></img>
                             </div>
                             <div className="col-md-6 p-4 ps-md-0">
                                 <div className="card">
